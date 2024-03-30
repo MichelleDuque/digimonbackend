db25fbc9a21e4b5457f679eb1996a1da9bad7c55
MichelleDuque <michelled_1994@hotmail.com>
2024-03-30 18:14:37 -0400

    deleted functions

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -33,38 +33,5 @@ class CharacterController extends Controller {
         return response()->json($character);
     }
     
-
-     public function save(Request $request) {
-        $this->validate($request, [
-            'title' => 'required',
-            'author_id' => 'required',
-            'published_date' => 'required|date',
-            'book_image' => 'required'
-        ]);
-        $book = Book::create($request->all());
-        return response()->json($book, 201);
-    }
-    
-    
-    
-    public function update(Request $request, $id) {
-        $book = Book::findOrFail($id);
-    
-        $this->validate($request, [
-            'title' => 'required',
-            'author_id' => 'required',
-            'published_date' => 'required|date',
-            'book_image' => 'required'
-        ]);
-        $book->update($request->all());
-        return response()->json($book);
-    }
-    
-    
-    public function delete($id) {
-        $book = Book::findOrFail($id);
-        $book->delete();
-        return response()->json(null, 204);
-    }
     
 }
